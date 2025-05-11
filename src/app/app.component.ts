3e6ee354a448fb342b5e026cf5fc9ef0e8aaa31e
Plepponium <kleinchmida@gmail.com>
2025-05-11 15:10:12 +0200

    updated skills section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,11 @@ import { ATFComponent } from "./atf/atf.component";
 import { HeaderComponent } from './shared/header/header.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { SkillsComponent } from './skills/skills.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ATFComponent, HeaderComponent, AboutMeComponent, SkillsComponent],
+  imports: [RouterOutlet, ATFComponent, HeaderComponent, AboutMeComponent, SkillsComponent, PortfolioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
